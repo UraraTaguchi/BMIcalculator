705dbc8bf72accde1970057eab5728b447430a49
Urara Taguchi <urara.rarara.0312@gmail.com>
2015-03-07 21:04:07 +0900

    Initial Commit

---
+++ BMIcalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMIcalculator
+//
+//  Created by 田口うらら on 2015/03/07.
+//  Copyright (c) 2015年 田口うらら. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
